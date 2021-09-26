6dfadd37b1447b054f65bb554706e07b8aeeca26
rdezerga <robert.dezerga@gmail.com>
2021-09-26 13:15:54 -0400

    a4 finish

--- a4scripts.js
+++ a4scripts.js
@@ -1,26 +1,23 @@
 
 
-    function calculateY(a, b, c, x) {
-        return a * x * x + b * x + c;
+    function calculateY(w, h) {
+        return (w / (h * h)) * 703;
     }
     
     function calculate() {
-        var a = Number($('#a').val());
-        var b = Number($('#b').val());
-        var c = Number($('#c').val());
+        var w = Number($('#w').val());
+        var h = Number($('#h').val());
         var xmin = Number($('#xmin').val());
         var xmax = Number($('#xmax').val());
-        var x = 0;
         var y = 0;
         
         var s = "";
         
-        s = "Y = " + a + " x<sup>2</sup> + ";
-        s+= b + " x + " + c + "<br/><br/>";
+        s = "BMI = (" + w + " / " + h + "<sup>2</sup>)" + " * 703" + "</br></br>";
         
-        for (x = xmin; x <= xmax; x++) {
-            y = calculateY(a, b, c, x);
-            s += " X = " + x + " Y = " + y + "<br/>";
+        for (w = xmin; w <= xmax; w++) {
+            y = calculateY(w, h);
+            s += "Weight: " + w + " || " + " Height: " + h + " || " + " BMI = " + " || " + y  + "</br>";
         }
         output.innerHTML = s;
     }
